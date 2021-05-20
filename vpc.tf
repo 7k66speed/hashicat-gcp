04026a0e8593c0da3626ec0e62941625a7b0ebe8
svenhashi <sven.kublun@db.com>
2021-05-20 10:01:16 +0000

    Fixed One More Naming Error in Network

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "network" {
   source  = "app.terraform.io/svenk-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name = "7knetwork"
+  network_name = "gaurav-network"
   project_id   = var.project
   subnets = [
     {
-      subnet_name   = "7knetwork-subnet"
+      subnet_name   = "gaurav-subnet"
       subnet_ip     = "10.100.10.0/24"
       subnet_region = var.region
     }
